22efa21a42159ac553f5b99ca1a276e8a43925ca
shahymerdan <54104146+shahymerdan@users.noreply.github.com>
2025-10-17 00:36:13 +0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Set variables
-PROJECT_ID=pro-pulsar-474309-v8
+PROJECT_ID=nifty-cabinet-475317-f8
 REGION=europe-west4
 IMAGE_NAME=pl
 ARTIFACT_REGISTRY_LOCATION=$REGION-docker.pkg.dev
 REPO_NAME=main-repo
-SERVICE_NAME=sppp
+SERVICE_NAME=spp
 VERSION_FILE=version.txt
 DEFAULT_VERSION=1
 
