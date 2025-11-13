2fbb7eea2c2079a70c286a9b2f5199c361aa8da6
shahymerdan <54104146+shahymerdan@users.noreply.github.com>
2025-11-14 01:37:15 +0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set variables
-PROJECT_ID=astral-sunbeam-478118-h9
+PROJECT_ID=central-run-478119-t4
 REGION=europe-west2
 IMAGE_NAME=pl
 ARTIFACT_REGISTRY_LOCATION=$REGION-docker.pkg.dev
