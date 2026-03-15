8b86128cf19b4e38d205e9bf987ec90de5110467
shahymerdan <54104146+shahymerdan@users.noreply.github.com>
2026-03-15 10:32:20 +0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ set -e  # Exit immediately if any command fails
 # -------------------------------
 # Configuration
 # -------------------------------
-PROJECT_ID="dcwozs24"
+PROJECT_ID="jufdmq25"
 REGION="europe-west4"
 IMAGE_NAME="pl"
 REPO_NAME="main-repo"
